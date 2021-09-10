d05d2bf114d7da258be8211a1bbbc6a7dcfff640
M Fachri <mfachri@upi.edu>
2021-09-10 22:17:11 +0700

    Update Matrix Selection.R

--- Matrix Selection.R
+++ Matrix Selection.R
@@ -8,7 +8,7 @@ non_us_all <- all_wars_matrix[,2]
 mean(non_us_all)
   
 # Select the non-US revenue for first two movies
-non_us_some <- all_wars_matrix[1:3,2]
+non_us_some <- all_wars_matrix[1:2,2]
   
 # Average non-US revenue for first two movies
 mean(non_us_some)
